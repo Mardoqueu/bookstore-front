edcc0c0f2342acf5fa92cc3a544312139a36284f
Mardoqueu Sousa <mdq767@gmail.com>
2022-12-15 11:38:00 -0300

    Componente LIVRO-CREATE: Implementando validacao nos formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { MatTableModule } from "@angular/material/table";
 import { MatButtonModule } from "@angular/material/button";
 import { CategoriaReadComponent } from "./components/views/categoria/categoria-read/categoria-read.component";
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
@@ -58,7 +58,8 @@ import { LivroCreateComponent } from './components/views/livro/livro-create/livr
     FormsModule,
     MatInputModule,
     MatFormFieldModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/views/livro/livro-create/livro-create.component.ts
+++ src/app/components/views/livro/livro-create/livro-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-livro-create',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LivroCreateComponent implements OnInit {
 
+  titulo = new FormControl('', [Validators.minLength(3)])
+  nome_autor = new FormControl('', [Validators.minLength(3)])
+  texto = new FormControl('', [Validators.minLength(10)])
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getMessage(){
+    if(this.titulo.invalid){
+      return 'O campo TITULO deve conter entre 3 e 100 caracteres';
+    }
+
+    if(this.nome_autor.invalid){
+      return 'O campo AUTOR deve conter entre 3 e 100 caracteres';
+    }
+
+    if(this.texto.invalid){
+      return 'O campo TEXTO deve conter entre 3 e 2000000 caracteres';
+    }
+    return false;
   }
 
 }
