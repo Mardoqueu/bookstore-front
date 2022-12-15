b8af69c722ece51a21a6be32511b1e774d06409d
Mardoqueu Sousa <mdq767@gmail.com>
2022-12-14 21:43:07 -0300

    Componente LIVRO-READ-ALL: consumindo dados

--- src/app/components/views/livros-read-all/livros-read-all.component.ts
+++ src/app/components/views/livros-read-all/livros-read-all.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LivroService } from '../livro/livro.service';
+import { Livro } from './livro.model';
 
 @Component({
   selector: 'app-livros-read-all',
@@ -9,9 +12,22 @@ export class LivrosReadAllComponent implements OnInit {
 
   displayedColumns: string[] = ["id", "titulo", "livros", "acoes"];
 
-  constructor() { }
+  id_cat: String = ""
+
+  livros: Livro[] = []
+
+  constructor(private service: LivroService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!;
+    this.findAll();
+  }
+
+  findAll(): void {
+    this.service.findAllByCategoria(this.id_cat).subscribe((resposta) => {
+      this.livros = resposta;
+      console.log(this.livros)
+    })
   }
 
 }
